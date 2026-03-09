61385efb4fcb1c06166a24d77e1a04a14f2804a2
Allan <allan.miljan@tptlive.ee>
2026-03-09 16:37:30 +0200

    import custom starting screen, create blank buttons

---
+++ screens/StartGameScreen.js
@@ -0,0 +1,13 @@
+import { TextInput, View } from 'react-native';
+
+import {PrimaryButton} from '../components/PrimaryButton';
+
+function StartGameScreen() {
+    return ( <View>
+        <TextInput/> Input
+        <PrimaryButton>Reset</PrimaryButton>
+    </View>
+    );
+}
+
+export default StartGameScreen;
