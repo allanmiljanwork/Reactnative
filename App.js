9a0bc3892e92e6272fd58f26e02a23540463de6a
Allan Miljan <allan.miljan@tptlive.ee>
2026-03-17 14:51:55 +0200

    init commit thridapp

--- App.js
+++ App.js
@@ -1,76 +1,20 @@
-import { useState } from 'react';
-import { StyleSheet, ImageBackground } from 'react-native';
-import { LinearGradient } from 'expo-linear-gradient';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import { useFonts } from 'expo-font';
-import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
-
-import StartGameScreen from './screens/StartGameScreen';
-import GameScreen from './screens/GameScreen';
-import GameOverScreen from './screens/GameOverScreen';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  const [userNumber, setUserNumber] = useState();
-  const [gameOver, setGameOver] = useState(true);
-  const [guessRounds, setGuessRounds] = useState(0);
-
-  const [fontsLoaded] = useFonts({
-    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
-    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf'),
-  });
-
-  if (!fontsLoaded) {
-    SplashScreen.hideAsync();
-  }
-
-
-
-  function pickNumberHandler(pickNumber) {
-    setUserNumber(pickNumber);
-    setGameOver(false);
-  }
-
-  function gameOverHandler(numberOfRounds) {
-    setGameOver(true);
-    setGuessRounds(numberOfRounds);
-  }
-
-  function startNewGameHandler() {
-    setUserNumber(null);
-    setGuessRounds(0);
-  }
-
-  let screen = <StartGameScreen onConfirmNumber={pickNumberHandler} />;
-
-  if (userNumber) {
-    screen = (<GameScreen userNumber={userNumber} onGameOver={gameOverHandler} />
-    );
-  }
-
-  if (gameOver && userNumber) {
-    screen = <GameOverScreen userNumber={userNumber} roundsNumber={guessRounds} onStartNewGame={startNewGameHandler} />;
-  }
-
   return (
-    <>
-    <StatusBar style='white' />
-    <LinearGradient colors={['indigo', 'gold']} style={styles.rootScreen}>
-      <ImageBackground source={require('./assets/images/dice.jpg')} resizeMode="cover" style={styles.rootScreen} imageStyle={styles.backgroundImage}>
-        <SafeAreaView style={styles.rootScreen}>
-          {screen}
-        </SafeAreaView>
-      </ImageBackground>
-    </LinearGradient>
-    </>
+    <View style={styles.container}>
+      <Text>Hello World!!!!</Text>
+      <StatusBar style="auto" />
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  rootScreen: {
-    flex: 1
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
-  backgroundImage: {
-    opacity: 0.35
-  }
 });
