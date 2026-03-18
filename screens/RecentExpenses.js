4142ecd5d67cb2a227422c448c84c382ddf135e0
Allan Miljan <allan.miljan@tptlive.ee>
2026-03-19 01:47:41 +0200

    add msg if there are no recent or any expense at all.

--- screens/RecentExpenses.js
+++ screens/RecentExpenses.js
@@ -7,15 +7,19 @@ import { getDateMinusDays } from "../util/date";
 function RecentExpenses() {
   const expensesCtx = useContext(ExpensesContext);
 
-  const RecentExpenses = expensesCtx.expenses.filter((expense) => {
+  const recentExpenses = expensesCtx.expenses.filter((expense) => {
     const today = new Date();
     const date7DaysAgo = getDateMinusDays(today, 7);
 
-    return expense.date > date7DaysAgo;
+    return expense.date >= date7DaysAgo && expense.date <= today;
   });
 
   return (
-    <ExpensesOutput expenses={RecentExpenses} expensesPeriod="Last 7 Days" />
+    <ExpensesOutput
+      expenses={recentExpenses}
+      expensesPeriod="Last 7 Days"
+      fallbackText="No expenses registered for the last 7 days."
+    />
   );
 }
 
