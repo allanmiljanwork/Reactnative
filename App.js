1e8eb300323dc05f8aa177d46413ef48f278652d
Allan Miljan <allan.miljan@tptlive.ee>
2026-03-18 23:17:43 +0200

    add component folder, styling constant, screen for tabs, display dummy flatlist data with styling

--- App.js
+++ App.js
@@ -1,21 +1,69 @@
-import { Text, View, StyleSheet } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
-export default function Index() {
+import ManageExpense from "./screens/ManageExpense";
+import RecentExpenses from "./screens/RecentExpenses";
+import AllExpenses from "./screens/AllExpenses";
+import { GlobalStyles } from "./constants/styles";
+
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import Entypo from "@expo/vector-icons/Entypo";
+
+const Stack = createNativeStackNavigator();
+const BottomTabs = createBottomTabNavigator();
+
+function ExpensesOverview() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Home screen</Text>
-    </View>
+    <BottomTabs.Navigator
+      screenOptions={{
+        headerStyle: { backgroundColor: GlobalStyles.colors.primary500 },
+        headerTintColor: "white",
+        tabBarStyle: { backgroundColor: GlobalStyles.colors.primary500 },
+        tabBarActiveTintColor: GlobalStyles.colors.accent500,
+      }}
+    >
+      <BottomTabs.Screen
+        name="RecentExpenses"
+        component={RecentExpenses}
+        options={{
+          title: "Recent Expenses",
+          tabBarLabel: "Recent",
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesome name="hourglass-o" size={size} color={color} />
+          ),
+        }}
+      />
+      <BottomTabs.Screen
+        name="AllExpenses"
+        component={AllExpenses}
+        options={{
+          title: "All Expenses",
+          tabBarLabel: "All Expenses",
+          tabBarIcon: ({ color, size }) => (
+            <Entypo name="calculator" size={size} color={color} />
+          ),
+        }}
+      />
+    </BottomTabs.Navigator>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#25292e",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  text: {
-    color: "#fff",
-  },
-});
+export default function App() {
+  return (
+    <>
+      <StatusBar style="auto" />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="ExpensesOverview"
+            component={ExpensesOverview}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen name="ManageExpense" component={ManageExpense} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
+  );
+}
---
+++ components/ExpensesOutput/ExpensesOutput.js
@@ -0,0 +1,47 @@
+import { View, StyleSheet } from "react-native";
+
+import ExpensesSummary from "./ExpensesSummary";
+import ExpensesList from "./ExpensesList";
+import { GlobalStyles } from "../../constants/styles";
+
+const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    description: "a pair of shoes",
+    amount: 59.99,
+    date: new Date("2021-12-19"),
+  },
+  {
+    id: "e2",
+    description: "phone",
+    amount: 300.99,
+    date: new Date("2022-12-19"),
+  },
+  {
+    id: "e3",
+    description: "chips",
+    amount: 1.99,
+    date: new Date("2024-12-19"),
+  },
+];
+
+function ExpensesOutput({ expenses, expensesPeriod }) {
+  return (
+    <View style={styles.container}>
+      <ExpensesSummary expenses={DUMMY_EXPENSES} periodName={expensesPeriod} />
+      <ExpensesList expenses={DUMMY_EXPENSES} />
+    </View>
+  );
+}
+
+export default ExpensesOutput;
+
+const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: 24,
+    paddingTop: 24,
+    paddingBottom: 0,
+    backgroundColor: GlobalStyles.colors.primary700,
+    flex: 1,
+  },
+});
