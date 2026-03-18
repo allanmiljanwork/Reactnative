b38636f55daa9af4d45834afb25d052b36e3bea0
Allan Miljan <allan.miljan@tptlive.ee>
2026-03-18 14:33:05 +0200

    refactor code from MealDetailScreen into reuseable chunks

---
+++ screens/MealDetailScreen.js
@@ -0,0 +1,57 @@
+import { Text, View, Image, StyleSheet } from "react-native";
+
+import { MEALS } from "../data/dummy-data";
+import MealDetails from "../components/MealDetails";
+import Subtitle from "../components/mealDetail/Subtitle";
+import List from "../components/mealDetail/List";
+
+function MealDetailScreen({ route }) {
+  const mealId = route.params.mealId;
+  const selectedMeal = MEALS.find((meal) => meal.id === mealId);
+
+  return (
+    <View>
+      <Image style={styles.image} source={{ uri: selectedMeal.imageUrl }} />
+      <Text style={styles.title}>{selectedMeal.title}</Text>
+      <MealDetails
+        duration={selectedMeal.duration}
+        complexity={selectedMeal.complexity}
+        affordability={selectedMeal.affordability}
+        textStyle={styles.detailText}
+      />
+      <View style={styles.listOuter}>
+        <View style={styles.listContainer}>
+          <Subtitle>ingredients</Subtitle>
+          <List data={selectedMeal.ingredients} />
+          <Subtitle>Steps</Subtitle>
+          <List data={selectedMeal.steps} />
+        </View>
+      </View>
+    </View>
+  );
+}
+
+export default MealDetailScreen;
+
+const styles = StyleSheet.create({
+  image: {
+    width: "100%",
+    height: 350,
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 24,
+    margin: 8,
+    textAlign: "center",
+    color: "white",
+  },
+  detailText: {
+    color: "white",
+  },
+  listOuter: {
+    alignItems: "center",
+  },
+  listContainer: {
+    maxWidth: "80%",
+  },
+});
