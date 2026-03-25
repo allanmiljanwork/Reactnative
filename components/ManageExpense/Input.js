95768c7d91edb7e3a4cf17f0ca60e72f4c24e2b8
Allan Miljan <allan.miljan@tptlive.ee>
2026-03-25 16:22:23 +0200

    add validation, add functionality to adding expense.

---
+++ components/ManageExpense/Input.js
@@ -0,0 +1,56 @@
+import { View, Text, TextInput, StyleSheet } from "react-native";
+
+import { GlobalStyles } from "../../constants/styles";
+
+function Input({ label, invalid, style, TextInputCfg }) {
+  const inputStyles = [styles.input];
+
+  if (TextInputCfg && TextInputCfg.multiline) {
+    inputStyles.push(styles.inputMultiline);
+  }
+
+  if (invalid) {
+    inputStyles.push(styles.invalidInput);
+  }
+
+  return (
+    <View style={[styles.inputContainer, style]}>
+      <Text style={[styles.label, invalid && styles.invalidLabel]}>
+        {label}
+      </Text>
+      <TextInput style={inputStyles} {...TextInputCfg} />
+    </View>
+  );
+}
+
+export default Input;
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    marginHorizontal: 4,
+    marginVertical: 8,
+    flex: 1,
+  },
+  label: {
+    fontSize: 12,
+    color: GlobalStyles.colors.primary100,
+    marginBottom: 4,
+  },
+  input: {
+    backgroundColor: GlobalStyles.colors.primary100,
+    color: GlobalStyles.colors.primary700,
+    padding: 6,
+    borderRadius: 6,
+    fontSize: 18,
+  },
+  inputMultiline: {
+    minHeight: 100,
+    textAlignVertical: "top",
+  },
+  invalidLabel: {
+    color: GlobalStyles.colors.error500,
+  },
+  invalidInput: {
+    color: GlobalStyles.colors.error50,
+  },
+});
