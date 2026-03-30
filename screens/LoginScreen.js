e2d46a0438c3979f7128efeb1c126e19b75f7c6c
Allan Miljan <allan.miljan@tptlive.ee>
2026-03-30 18:52:10 +0300

    add auth for login

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,16 +1,25 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
+import { Alert } from "react-native";
 
 import AuthContent from "../components/Auth/AuthContent";
 import LoadingOverlay from "../components/ui/LoadingOverlay";
 import { login } from "../util/auth";
+import { AuthContent as AuthContext } from "../store/auth-context";
 
 function LoginScreen() {
   const [isAuthenticating, setIsAuthenticting] = useState(false);
 
+  const authCtx = useContext(AuthContext);
+
   async function loginHandler({ email, password }) {
     setIsAuthenticting(true);
-    await login(email, password);
-    setIsAuthenticting(false);
+    try {
+      const token = await login(email, password);
+      authCtx.authenticate(token);
+    } catch (error) {
+      Alert.alert("Authentication failed!", "Could not log you in");
+      setIsAuthenticting(false);
+    }
   }
 
   if (isAuthenticating) {
---
+++ store/auth-context.js
@@ -0,0 +1,31 @@
+import { createContext, useState } from "react";
+
+export const AuthContent = createContext({
+  token: "",
+  isAuthenticated: false,
+  authenticate: (token) => {},
+  logout: () => {},
+});
+
+function AuthContextProvider({ children }) {
+  const [authToken, setAuthToken] = useState();
+
+  function authenticate(token) {
+    setAuthToken(token);
+  }
+
+  function logout() {
+    setAuthToken(null);
+  }
+
+  const value = {
+    token: authToken,
+    isAuthenticated: !!authToken,
+    authenticate: authenticate,
+    logout: logout,
+  };
+
+  return <AuthContent.Provider value={value}>{children}</AuthContent.Provider>;
+}
+
+export default AuthContextProvider;
