e3e8065a638047b1b945db1e97f584625e240fc4
Allan Miljan <allan.miljan@tptlive.ee>
2026-04-01 10:43:58 +0300

    add sign up token auth

--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -1,16 +1,25 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
+import { Alert } from "react-native";
 
 import AuthContent from "../components/Auth/AuthContent";
 import { createUser } from "../util/auth";
 import LoadingOverlay from "../components/ui/LoadingOverlay";
+import { AuthContext } from "../store/auth-context";
 
 function SignUpScreen() {
   const [isAuthenticating, setIsAuthenticting] = useState(false);
 
+  const authCtx = useContext(AuthContext);
+
   async function signUpHandler({ email, password }) {
     setIsAuthenticting(true);
-    await createUser(email, password);
-    setIsAuthenticting(false);
+    try {
+      const token = await createUser(email, password);
+      authCtx.authenticate(token);
+    } catch (error) {
+      Alert.alert("Auth failed", "Can't register");
+      setIsAuthenticting(false);
+    }
   }
 
   if (isAuthenticating) {
