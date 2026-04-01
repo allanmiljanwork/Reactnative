4c38213cafbed44ddca451d4037f5c6eadafee92
Allan <allan.miljan@tptlive.ee>
2026-04-01 22:43:36 +0300

    add place screen

--- components/Places/PlacesList.js
+++ components/Places/PlacesList.js
@@ -1,14 +1,12 @@
-import { FlatList, StyleSheet, Text, View } from 'react-native';
+import { FlatList, StyleSheet, Text, View } from "react-native";
 
-import PlaceItem from './PlaceItem';
+import PlaceItem from "./PlaceItem";
 
 function PlacesList({ places }) {
   if (!places || places.length === 0) {
     return (
       <View style={styles.fallbackContainer}>
-        <Text style={styles.fallbackText}>
-          No places added yet - start adding some!
-        </Text>
+        <Text style={styles.fallbackText}>add place</Text>
       </View>
     );
   }
@@ -27,8 +25,8 @@ export default PlacesList;
 const styles = StyleSheet.create({
   fallbackContainer: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
   fallbackText: {
     fontSize: 16,
