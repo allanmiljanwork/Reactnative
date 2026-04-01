25a67c2d144a250e3bff40c5c1a7589bed851858
Allan <allan.miljan@tptlive.ee>
2026-04-01 23:21:19 +0300

    add colors

--- components/UI/IconButton.js
+++ components/UI/IconButton.js
@@ -1,5 +1,5 @@
-import { Pressable, StyleSheet } from 'react-native';
-import { Ionicons } from '@expo/vector-icons';
+import { Pressable, StyleSheet } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
 
 function IconButton({ icon, size, color, onPress }) {
   return (
@@ -17,9 +17,8 @@ export default IconButton;
 const styles = StyleSheet.create({
   button: {
     padding: 8,
-    margin: 4,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
   pressed: {
     opacity: 0.7,
