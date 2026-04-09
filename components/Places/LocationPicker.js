ecde80370a2048c023572e55da059c31186f2377
Allan Miljan <allan.miljan@tptlive.ee>
2026-04-09 17:39:27 +0300

    add google static api

--- components/Places/LocationPicker.js
+++ components/Places/LocationPicker.js
@@ -1,4 +1,5 @@
-import { View, StyleSheet, Button, Alert } from "react-native";
+import { useState } from "react";
+import { Alert, View, StyleSheet, Image, Text, Button } from "react-native";
 import {
   getCurrentPositionAsync,
   useForegroundPermissions,
@@ -6,8 +7,11 @@ import {
 } from "expo-location";
 
 import { Colors } from "../../constants/colors";
+import { getMapPreview } from "../../util/location";
 
 function LocationPicker() {
+  const [pickedLocation, setPickedLocation] = useState();
+
   const [locationPerms, requestPerms] = useForegroundPermissions();
 
   async function verifyPermissions() {
@@ -36,21 +40,41 @@ function LocationPicker() {
     }
 
     const location = await getCurrentPositionAsync();
-    console.log(location);
+    setPickedLocation({
+      lat: location.coords.latitude,
+      lng: location.coords.longitude,
+    });
   }
 
   function pickOnMapHandler() {}
 
+  let locationPreview = <Text>No location picked yet.</Text>;
+
+  if (pickedLocation) {
+    locationPreview = (
+      <Image
+        style={styles.image}
+        source={{
+          uri: getMapPreview(pickedLocation.lat, pickedLocation.lng),
+        }}
+      />
+    );
+  }
+
   return (
     <View>
-      <View style={styles.mapPreview}></View>
+      <View style={styles.mapPreview}>{locationPreview}</View>
       <View style={styles.actions}>
-        <Button title="user location" onPress={getLocationHandler}>
-          Locate User
-        </Button>
-        <Button title="map" onPress={pickOnMapHandler}>
-          Pick on Map
-        </Button>
+        <Button
+          title="Locate User"
+          icon="location"
+          onPress={getLocationHandler}
+        ></Button>
+        <Button
+          title="Pick on Map"
+          icon="map"
+          onPress={pickOnMapHandler}
+        ></Button>
       </View>
     </View>
   );
@@ -67,10 +91,15 @@ const styles = StyleSheet.create({
     alignItems: "center",
     backgroundColor: Colors.primary100,
     borderRadius: 4,
+    overflow: "hidden",
   },
   actions: {
     flexDirection: "row",
     justifyContent: "space-around",
     alignItems: "center",
+  },
+  image: {
+    width: "100%",
+    height: "100%",
   },
 });
--- util/location.js
+++ util/location.js
@@ -1,1 +1,6 @@
-// implement https://leafletjs.com/ to replace google api maps
+import API_KEY from "../cfg";
+
+export function getMapPreview(lat, lng) {
+  const imagePreviewUrl = `https://maps.googleapis.com/maps/api/staticmap?center=${lat},${lng}&zoom=14&size=400x200&maptype=roadmap&markers=color:red%7Clabel:S%7C${lat},${lng}&key=${API_KEY}`;
+  return imagePreviewUrl;
+}
