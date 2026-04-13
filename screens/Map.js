c320d54d648d825e79feec85536cf7814df51935
Allan Miljan <allan.miljan@tptlive.ee>
2026-04-13 10:24:51 +0300

    Confirming Picked Locations

--- screens/Map.js
+++ screens/Map.js
@@ -1,7 +1,9 @@
-import { StyleSheet } from "react-native";
+import { useState, useCallback } from "react";
+import { StyleSheet, Button } from "react-native";
 import MapView, { Marker } from "react-native-maps";
 
-function Map() {
+function Map({ navigation }) {
+  const [selectLocation, setSelectLocation] = useState();
   const region = {
     latitude: 37.78,
     longitude: -122.43,
@@ -9,7 +11,59 @@ function Map() {
     longitudeDelta: 0.0421,
   };
 
-  return <MapView style={styles.map} initialRegion={region}></MapView>;
+  function selectLocationHandler(event) {
+    console.log(event);
+    const lat = event.nativeEvent.coordinate.latitude;
+    const lng = event.nativeEvent.coordinate.longitude;
+
+    setSelectLocation({ lat: lat, lng: lng });
+  }
+
+  const savePickedLocationHandler = useCallback(() => {
+    if (!selectLocation) {
+      Alert.alert(
+        "No location picked!",
+        "You have to pick a location (by tapping the screen) first!",
+      );
+      return;
+    }
+
+    navigation.navigate("addPlace", {
+      pickedLat: selectLocation.lat,
+      pickedLng: selectLocation.lng,
+    });
+  }, [navigation, selectLocation]);
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerRight: ({ tintColor }) => (
+        <Button
+          icon="save"
+          size={24}
+          color={tintColor}
+          onPress={savePickedLocationHandler}
+        />
+      ),
+    });
+  }, [navigation, savePickedLocationHandler]);
+
+  return (
+    <MapView
+      style={styles.map}
+      initialRegion={region}
+      onPress={selectLocationHandler}
+    >
+      {selectLocation && (
+        <Marker
+          title="Picked Location"
+          coordinate={{
+            latitude: selectLocation.lat,
+            longitude: selectLocation.lng,
+          }}
+        />
+      )}
+    </MapView>
+  );
 }
 
 export default Map;
